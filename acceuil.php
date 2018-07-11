3328bff75f20f6323660d1d71d27aa4c48cef56f
Margotine <margaux.thoquenne.sio@gmail.com>
2018-07-11 21:46:14 +0200

    1er Commit
    premier commit

---
+++ acceuil.php
@@ -0,0 +1,27 @@
+<?php
+
+
+include("head.php");
+
+
+
+?>
+<body class="img">
+	<section id="titreTop">
+		<div class="title">
+			<h1 >Bienvenue chez Hervé et Elodie </h1>
+		</div>
+	</section>
+	
+	<?php
+
+
+include("nav.php");
+
+
+
+?>
+
+	</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<div class="clear"> </div>
+<section id="nav">
+	<div class="navbar">
+	  <a href="#home">Home</a>
+	  <a href="#news">News</a>
+	  <a href="#contact">Contact</a>
+	</div>
+</section>
